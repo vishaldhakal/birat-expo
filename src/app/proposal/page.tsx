0329111cf5c6003c15d668e2ef988b0fadd1b124
sazal <sazals48@gmail.com>
2026-09-17 10:17:05 +0545

    update proposal

--- src/app/proposal/page.tsx
+++ src/app/proposal/page.tsx
@@ -6,11 +6,11 @@ export default function Proposal() {
       <div className="pt-20 pb-40 flex justify-center">
         <div className="flex justify-center flex-col items-center">
           <h2 className="text-4xl sm:text-5xl md:text-5xl font-black text-transparent bg-clip-text bg-gradient-to-r from-blue-600 to-purple-600 uppercase mb-10">
-            Birat Expo 2025 Proposal
+            Birat Expo 2026 Proposal
           </h2>
           <div className="flex justify-center my-6">
             <a
-              href="/Contract_Sponsorships_Birat_Expo_2024.docx.pdf"
+              href="/birat-expo-2026/Proposal 2026.pdf"
               download
               className="px-4 py-2 text-white bg-blue-600 rounded-md no-underline"
             >
@@ -19,7 +19,7 @@ export default function Proposal() {
           </div>
           <object
             className="pdf"
-            data="/Birat Expo 2025 - Proposal.pdf"
+            data="/birat-expo-2026/Proposal 2026.pdf"
             width="800"
             height="750"
           ></object>
